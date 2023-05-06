b10c6a6c8fba97b5faf16fae0de2ff996af8e817
hussainkeshwani61 <your_email_address@example.com>
2023-05-06 11:28:02 +0100

    Route for Dashboard added

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
 Route::post('/login', 'App\Http\Controllers\Auth\LoginController@login')->name('login');
 Route::post('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
-
-// Registration Routes...
 Route::post('/register', 'App\Http\Controllers\Auth\RegisterController@register')->name('register');
+Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
+
